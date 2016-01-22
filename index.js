60b07069d6e37949c0c0d364c03a2096fb43ad3d
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-01-22 19:57:19 +0100

    Support cb & promises together ✌️

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const pify = require('pify')
+const util = require('util')
 const async = require('async')
 
 const EXCLUDE_METHODS = [ 'apply', 'memoize', 'log', 'dir', 'noConflict' ]
@@ -10,13 +11,22 @@ function remove (arr, element) {
   index !== -1 && arr.splice(index, 1)
 }
 
-let methods = Object.keys(async)
+const methods = Object.keys(async)
 
 EXCLUDE_METHODS.forEach(function (exclude) {
   remove(methods, exclude)
 })
 
-module.exports = methods.reduce(function (accumulator, method) {
+const asyncPromise = methods.reduce(function (accumulator, method) {
   accumulator[method] = pify(async[method])
+  return accumulator
+}, {})
+
+module.exports = methods.reduce(function (accumulator, method) {
+  accumulator[method] = (function () {
+    const cb = arguments[arguments.length - 1]
+    const proxy = util.isFunction(cb) ? async : asyncPromise
+    return proxy[method]
+  })()
   return accumulator
 }, {})
